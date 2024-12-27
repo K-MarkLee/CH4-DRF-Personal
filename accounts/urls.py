f6d59a6dfdb4ed1410c8686548dc8ca17533e43f
K-MarkLee <seungyeol6825@gmail.com>
2024-12-27 19:36:44 +0900

    add : accounts > login / views, urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create/', views.create_user, name='create_user'),
+    path('create/', views.create_user, name='create_user'), # 회원가입
+    path('login/', views.login, name='login'), # 로그인
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,13 @@
 from rest_framework import status
 from rest_framework.response import Response
+from django.contrib.auth import authenticate
 from .serializers import UserCreateSerializer
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 
 
-
+User = get_user_model()
 # Create your views here.
 
 @api_view(['POST'])
@@ -17,3 +20,26 @@ def create_user(request):
         return Response({"message": "Account created successfully."}, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+
+@api_view(['POST'])
+@authentication_classes([])  # 인증 비활성화
+@permission_classes([])      # 권한 비활성화
+def login(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    # 사용자 인증
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None: 
+        # 인증 성공: 토큰 발급(JWT 토큰 생성)
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            "access": str(refresh.access_token),
+            "refresh": str(refresh),
+            "message": "Login successful",
+        }, status=status.HTTP_200_OK)
+    else:
+        # 인증 실패
+        return Response({"error": "Invalid username or password"}, status=status.HTTP_401_UNAUTHORIZED)
