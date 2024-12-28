bd92f425793755fe59969ef1d7addda0c6cfffbe
K-MarkLee <seungyeol6825@gmail.com>
2024-12-28 18:35:25 +0900

    add : list_products > urls, views

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('create_product/', views.create_product, name='create_product'), # 상품 생성
+    path('list_products/', views.list_products, name='list_products'), # 상품 목록
 ]
--- products/views.py
+++ products/views.py
@@ -3,8 +3,7 @@ from rest_framework import status
 from django.shortcuts import render
 from .serializers import ProductSerializer
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view
 
 # Create your views here.
 
@@ -17,3 +16,10 @@ def create_product(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET']) # GET 요청만 허용
+def list_products(request):
+    products = Product.objects.all().order_by("created_at") # 모든 상품 조회 (작성일 기준)
+    serializer = ProductSerializer(products, many=True) 
+    return Response(serializer.data, status=status.HTTP_200_OK) # 상품 목록 반환
