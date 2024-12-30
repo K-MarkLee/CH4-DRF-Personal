d5d835a42d307a1fd0c58259bec8395edc12d2a8
K-MarkLee <seungyeol6825@gmail.com>
2024-12-30 12:38:23 +0900

    test

--- products/serializers.py
+++ products/serializers.py
@@ -7,7 +7,11 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = ['id', 'author', 'title', 'content', 'image', 'created_at', 'updated_at'] # 필드
         read_only_fields = ['id', 'author', 'created_at', 'updated_at'] # 읽기 전용 필드
 
-    def validate_image(self, data):
-        if not data:
-            raise serializers.ValidationError("An image is required for this product.")
+    def validate(self, data):
+
+        if not data.get('image'):
+            raise serializers.ValidationError({"image": "An image is required for this product."})
+        
+        if 'image' in data and data['image'] is None:
+            raise serializers.ValidationError({"image": "An image is required for this product."})
         return data
